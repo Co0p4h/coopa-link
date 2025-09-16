f2d4e770d10ba90895fb9368c5ec38d59203d0f8
coopa <cooper.pulver@gmail.com>
2025-09-16 12:02:37 +1000

    lskdjflksdjflkas

--- src/index.ts
+++ src/index.ts
@@ -96,7 +96,7 @@ app.notFound((c) => {
 
 serve({
   fetch: app.fetch,
-  port: parseInt(process.env.PORT!) || 8080
+  port: 8080
 }, (info) => {
   console.log(`server is running on http://localhost:${info.port}`);
 })
